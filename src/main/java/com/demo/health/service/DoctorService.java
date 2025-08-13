3a74470ae75707b388098e555cedfd8681680932
rohan-devs <rohanshah6982@gmail.com>
2025-08-13 12:01:56 +0530

    service layer added

---
+++ src/main/java/com/demo/health/service/DoctorService.java
@@ -0,0 +1,19 @@
+package com.demo.health.service;
+
+import java.util.List;
+
+import com.demo.health.entity.Doctor;
+
+public interface DoctorService {
+
+	void save(Doctor doctor);
+
+	Doctor get(int doctorId);
+
+	List<Doctor> list();
+
+	void update(Doctor doctor);
+
+	void delete(int doctorId);
+
+}
---
+++ src/main/java/com/demo/health/service/PatientService.java
@@ -0,0 +1,19 @@
+package com.demo.health.service;
+
+import java.util.List;
+
+import com.demo.health.entity.Patient;
+
+public interface PatientService {
+
+	void save(Patient patient);
+
+	Patient get(int patientId);
+
+	List<Patient> list();
+
+	void update(Patient patient);
+
+	void delete(int patientId);
+
+}
