bdff7af5e6996b3e1f7e8f3e15e52ae720c5dfd1
RajSingh-45 <singhraj45official@gmail.com>
2025-08-13 12:33:04 +0530

    Initial Controller

---
+++ src/main/java/com/demo/health/controller/InitialController.java
@@ -0,0 +1,14 @@
+package com.demo.health.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class InitialController {
+	@RequestMapping("/")
+	public String start() {
+		return "employees";
+	}
+}
