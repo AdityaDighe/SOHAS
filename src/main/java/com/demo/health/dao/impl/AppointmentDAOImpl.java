0ee6f363215c766b16cecd5efdff137acb2a0a16
AdityaDighe <ardighe7@gmail.com>
2025-08-13 11:54:10 +0530

    Impl files adding

--- src/main/java/com/demo/health/dao/impl/AppointmentDAOImpl.java
+++ src/main/java/com/demo/health/dao/impl/AppointmentDAOImpl.java
@@ -1,5 +1,40 @@
 package com.demo.health.dao.impl;
 
-public class AppointmentDAOImpl {
+import com.demo.health.dao.AppointmentDAO;
+import com.demo.health.entity.Appointment;
 
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+@Transactional
+public class AppointmentDAOImpl implements AppointmentDAO{
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public void save(Appointment appointment) {
+		sessionFactory.getCurrentSession().save(appointment);
+	}
+
+	public Appointment get(int appointmentId) {
+		return sessionFactory.getCurrentSession().get(Appointment.class, appointmentId);
+	}
+
+	public List<Appointment> list() {
+		return sessionFactory.getCurrentSession().createQuery("from Appointment", Appointment.class).list();
+	}
+
+	public void update(Appointment appointment) {
+		sessionFactory.getCurrentSession().update(appointment);
+	}
+
+	public void delete(int appointmentId) {
+		Appointment a = get(appointmentId);
+		if (a != null)
+			sessionFactory.getCurrentSession().delete(a);
+	}
 }
--- src/main/java/com/demo/health/dao/impl/PatientDAOImpl.java
+++ src/main/java/com/demo/health/dao/impl/PatientDAOImpl.java
@@ -1,5 +1,40 @@
 package com.demo.health.dao.impl;
 
-public class PatientDAOImpl {
+import com.demo.health.dao.PatientDAO;
+import com.demo.health.entity.Patient;
 
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+@Transactional
+public class PatientDAOImpl implements PatientDAO{
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public void save(Patient patient) {
+		sessionFactory.getCurrentSession().save(patient);
+	}
+
+	public Patient get(int patientId) {
+		return sessionFactory.getCurrentSession().get(Patient.class, patientId);
+	}
+
+	public List<Patient> list() {
+		return sessionFactory.getCurrentSession().createQuery("from Patient", Patient.class).list();
+	}
+
+	public void update(Patient patient) {
+		sessionFactory.getCurrentSession().update(patient);
+	}
+
+	public void delete(int patientId) {
+		Patient p = get(patientId);
+		if (p != null)
+			sessionFactory.getCurrentSession().delete(p);
+	}
 }
