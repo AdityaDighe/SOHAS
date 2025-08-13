16a63b0c3586052579a0bccba44f7c1f0c5ae86d
AdityaDighe <ardighe7@gmail.com>
2025-08-13 12:37:40 +0530

    Patient Controller

--- src/main/java/com/demo/health/service/DoctorService.java
+++ src/main/java/com/demo/health/service/DoctorService.java
@@ -5,15 +5,9 @@ import java.util.List;
 import com.demo.health.entity.Doctor;
 
 public interface DoctorService {
-
 	void save(Doctor doctor);
-
 	Doctor get(int doctorId);
-
 	List<Doctor> list();
-
 	void update(Doctor doctor);
-
 	void delete(int doctorId);
-
 }
--- src/main/java/com/demo/health/service/PatientService.java
+++ src/main/java/com/demo/health/service/PatientService.java
@@ -5,15 +5,9 @@ import java.util.List;
 import com.demo.health.entity.Patient;
 
 public interface PatientService {
-
 	void save(Patient patient);
-
 	Patient get(int patientId);
-
 	List<Patient> list();
-
 	void update(Patient patient);
-
 	void delete(int patientId);
-
 }
